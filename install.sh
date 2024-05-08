59b184b44d446371c4bae9220468a8242e48280f
Sarah <com@chootka.com>
2024-05-08 12:43:39 +0200

    Update install.sh for RPi 3A+

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ clear
 echo "Installing Subnodes..."
 echo ""
 
-read -p "This script will install ipfs, nginx, set up a wireless access point and captive portal with hostapd and dnsmasq, and provide the option of configuring a BATMAN-ADV mesh point with batctl. Make sure you have one (or two, if installing the additional mesh point) USB wifi radios connected to your Raspberry Pi before proceeding. Press any key to continue..."
+read -p "This script will install ipfs, nginx, set up a wireless access point and captive portal with hostapd and dnsmasq. Make sure you have an additional USB wifi radios connected to your Raspberry Pi before proceeding. Press any key to continue..."
 echo ""
 clear
 
@@ -45,35 +45,17 @@ clear
 # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # CHECK REQUIRED RADIOS
 # check that iw list does not fail with 'nl80211 not found'
-echo -en "Checking that USB wifi radio is available for access point..."
-readarray IW < <(iw dev | awk '$1~"phy#"{PHY=$1}; $1=="Interface" && $2~"wlan"{WLAN=$2; sub(/#/, "", PHY); print PHY " " WLAN}')
+echo -en "Checking that USB wifi radio is available for mesh point..."
+readarray IW < <(iw dev | awk '$1~"phy#"{PHY=$1}; $1=="Interface" && $2!="wlan0"{WLAN=$2; sub(/#/, "", PHY); print PHY " " WLAN}')
+
 if [[ -z $IW ]] ; then
-	echo -en "[FAIL]\n"
-	echo "Warning! Wireless adapter not found! Please plug in a wireless radio after installation completes and before reboot."
+	echo -en "[FAIL]\n"		
+ 	echo "Warning! Second wireless adapter not found! Please plug in an addition wireless radio after installation completes and before reboot."
 	echo "Installation process will proceed in 5 seconds..."
 	sleep 5
 else
 	echo -en "[OK]\n"
 fi
-
-# now check that iw list finds a radio other than wlan0 if mesh point option was set to 'y' in config file
-case $DO_SET_MESH in
-	[Yy]* )
-		echo -en "Checking that USB wifi radio is available for mesh point..."
-		readarray IW < <(iw dev | awk '$1~"phy#"{PHY=$1}; $1=="Interface" && $2!="wlan0"{WLAN=$2; sub(/#/, "", PHY); print PHY " " WLAN}')
-
-		if [[ -z $IW ]] ; then
-			echo -en "[FAIL]\n"
-			echo "Warning! Second wireless adapter not found! Please plug in an addition wireless radio after installation completes and before reboot."
-			echo "Installation process will proceed in 5 seconds..."
-			sleep 5
-		else
-			echo -en "[OK]\n"
-		fi
-;;
-esac
-
-
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -81,8 +63,8 @@ esac
 #
 
 # update the packages
-echo -en "Updating apt and installing iw, dnsutils, nginx, batctl, tar, wget"
-apt update && apt install -y iw dnsutils nginx batctl tar wget
+echo -en "Updating apt and installing iw, dnsutils, nginx, tar, wget"
+apt update && apt install -y iw dnsutils nginx tar wget
 # Change the directory owner and group
 chown www-data:www-data /var/www
 # allow the group to write to the directory
@@ -161,52 +143,32 @@ EOF
 	fi
 
 
-
-
 # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Check if we are configuring a mesh node
+# Access Point only
+#
 
-clear
-echo -en "Checking whether to configure mesh point or not..."
+# configure dnsmasq
+echo -en "Creating dnsmasq configuration file..."
+cat <<EOF > /etc/dnsmasq.conf
+# Captive Portal logic (redirects traffic coming in on br0 to our web server)
+interface=wlan1
+address=/#/$AP_IP
 
-case $DO_SET_MESH in
-	[Yy]* )
-		clear
-		echo -en "Configuring Raspberry Pi as a BATMAN-ADV mesh point..."
-		echo -en "Enabling the batman-adv kernel module..."
-		# add the batman-adv module to be started on boot
-		sed -i '$a batman-adv' /etc/modules
-		modprobe batman-adv;
-
-		# pass the selected mesh ssid into mesh startup script
-		sed -i "s/MTU/$MTU/" scripts/subnodes_mesh.sh
-		sed -i "s/SSID/$MESH_SSID/" scripts/subnodes_mesh.sh
-		sed -i "s/CELL_ID/$CELL_ID/" scripts/subnodes_mesh.sh
-		sed -i "s/CHAN/$MESH_CHANNEL/" scripts/subnodes_mesh.sh
-		sed -i "s/GW_MODE/$GW_MODE/" scripts/subnodes_mesh.sh
-		#sed -i "s/GW_SEL_CLASS/$GW_SEL_CLASS/" scripts/subnodes_mesh.sh
-		#sed -i "s/GW_BANDWIDTH/$GW_BANDWIDTH/" scripts/subnodes_mesh.sh
-		#sed -i "s/GW_IP/$GW_IP/" scripts/subnodes_mesh.sh
-
-		# configure dnsmasq
-		echo -en "Creating dnsmasq configuration file..."
-		cat <<EOF > /etc/dnsmasq.conf
-interface=br0
-address=/#/$BRIDGE_IP
-dhcp-range=$BR_DHCP_START,$BR_DHCP_END,$DHCP_NETMASK,$DHCP_LEASE
+# DHCP server
+dhcp-range=$AP_DHCP_START,$AP_DHCP_END,$DHCP_NETMASK,$DHCP_LEASE
 EOF
 	rc=$?
 	if [[ $rc != 0 ]] ; then
-    		echo -en "[FAIL]\n"
+		echo -en "[FAIL]\n"
 		echo ""
 		exit $rc
 	else
 		echo -en "[OK]\n"
 	fi
 
-		# create new /etc/network/interfaces
-		echo -en "Creating new network interfaces with your settings..."
-		cat <<EOF > /etc/network/interfaces
+# create new /etc/network/interfaces
+echo -en "Creating new network interfaces with your settings..."
+cat <<EOF > /etc/network/interfaces
 auto lo
 iface lo inet loopback
 
@@ -215,125 +177,28 @@ auto eth0
 iface eth0 inet dhcp
 
 auto wlan0
-iface wlan0 inet static
-address $AP_IP
-netmask $AP_NETMASK
+iface wlan0 inet dhcp
 
-auto br0
-iface br0 inet static
-address $BRIDGE_IP
-netmask $BRIDGE_NETMASK
-bridge_ports bat0 wlan0
-bridge_stp off
-
-iface default inet dhcp
-EOF
-		rc=$?
-		if [[ $rc != 0 ]] ; then
-		    	echo -en "[FAIL]\n"
-			echo ""
-			exit $rc
-		else
-			echo -en "[OK]\n"
-		fi
-
-		# create hostapd configuration with user's settings
-		echo -en "Creating hostapd.conf file..."
-		cat <<EOF > /etc/hostapd/hostapd.conf
-interface=wlan0
-bridge=br0
-driver=$RADIO_DRIVER
-country_code=$AP_COUNTRY
-ctrl_interface=/var/run/hostapd
-ctrl_interface_group=0
-ssid=$AP_SSID
-hw_mode=g
-channel=$AP_CHAN
-beacon_int=100
-auth_algs=1
-wpa=0
-ap_isolate=1
-macaddr_acl=0
-wmm_enabled=1
-ieee80211n=1
-EOF
-		rc=$?
-		if [[ $rc != 0 ]] ; then
-			echo -en "[FAIL]\n"
-			exit $rc
-		else
-			echo -en "[OK]\n"
-		fi
-
-		# COPY OVER START UP SCRIPTS
-		echo ""
-		echo "Adding startup scripts to init.d..."
-		cp scripts/subnodes_ap.sh /etc/init.d/subnodes_ap
-		chmod 755 /etc/init.d/subnodes_ap
-		update-rc.d subnodes_ap defaults
-		cp scripts/subnodes_mesh.sh /etc/init.d/subnodes_mesh
-		chmod 755 /etc/init.d/subnodes_mesh
-		update-rc.d subnodes_mesh defaults
-	;;
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Access Point only
-#
-
-	[Nn]* )
-	# if no mesh point is created, set up network interfaces, hostapd and dnsmasq to operate without a bridge
-		clear
-
-		# configure dnsmasq
-		echo -en "Creating dnsmasq configuration file..."
-		cat <<EOF > /etc/dnsmasq.conf
-# Captive Portal logic (redirects traffic coming in on br0 to our web server)
-interface=wlan0
-address=/#/$AP_IP
-
-# DHCP server
-dhcp-range=$AP_DHCP_START,$AP_DHCP_END,$DHCP_NETMASK,$DHCP_LEASE
-EOF
-		rc=$?
-		if [[ $rc != 0 ]] ; then
-	    		echo -en "[FAIL]\n"
-			echo ""
-			exit $rc
-		else
-			echo -en "[OK]\n"
-		fi
-
-		# create new /etc/network/interfaces
-		echo -en "Creating new network interfaces with your settings..."
-		cat <<EOF > /etc/network/interfaces
-auto lo
-iface lo inet loopback
-
-allow-hotplug eth0
-auto eth0
-iface eth0 inet dhcp
-
-auto wlan0
-iface wlan0 inet static
+auto wlan1
+iface wlan1 inet static
 address $AP_IP
 netmask $AP_NETMASK
 
 iface default inet dhcp
 EOF
-		rc=$?
-		if [[ $rc != 0 ]] ; then
-		    	echo -en "[FAIL]\n"
+	rc=$?
+	if [[ $rc != 0 ]] ; then
+		    echo -en "[FAIL]\n"
 			echo ""
-			exit $rc
-		else
-			echo -en "[OK]\n"
-		fi
+		exit $rc
+	else
+		echo -en "[OK]\n"
+	fi
 
-		# create hostapd configuration with user's settings
-		echo -en "Creating hostapd.conf file..."
-		cat <<EOF > /etc/hostapd/hostapd.conf
-interface=wlan0
+# create hostapd configuration with user's settings
+echo -en "Creating hostapd.conf file..."
+cat <<EOF > /etc/hostapd/hostapd.conf
+interface=wlan1
 driver=$RADIO_DRIVER
 country_code=$AP_COUNTRY
 ctrl_interface=/var/run/hostapd
@@ -348,22 +213,20 @@ macaddr_acl=0
 wmm_enabled=1
 ieee80211n=1
 EOF
-		rc=$?
-		if [[ $rc != 0 ]] ; then
-			echo -en "[FAIL]\n"
-			exit $rc
-		else
-			echo -en "[OK]\n"
-		fi
+	rc=$?
+	if [[ $rc != 0 ]] ; then
+		echo -en "[FAIL]\n"
+		exit $rc
+	else
+		echo -en "[OK]\n"
+	fi
 
-		# COPY OVER START UP SCRIPTS
-		echo ""
-		echo "Adding startup scripts to init.d..."
-		cp scripts/subnodes_ap.sh /etc/init.d/subnodes_ap
-		chmod 755 /etc/init.d/subnodes_ap
-		update-rc.d subnodes_ap defaults
-	;;
-esac
+# COPY OVER START UP SCRIPTS
+echo ""
+echo "Adding startup scripts to init.d..."
+cp scripts/subnodes_ap.sh /etc/init.d/subnodes_ap
+chmod 755 /etc/init.d/subnodes_ap
+update-rc.d subnodes_ap defaults
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # #
